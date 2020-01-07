7785ded1dd9896c4924519f922949ac12ef63f80
David White <dwhite0627@gmail.com>
2020-01-06 22:11:05 -0500

    Add API data using axios

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import Photo from './Photo';
 
-const testURL = 'https://apod.nasa.gov/apod/image/2001/BetelgeuseImagined_EsoCalcada_960.jpg';
+const apiKey = 'DEMO_KEY';
+const apiURL = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`;
 
 const Photos = (props) => {
+  const [obj, setObj] = useState();
+  useEffect(() => {
+    axios
+      .get(apiURL)
+      .then(res => {
+        setObj(res)
+      })
+      .catch(err => console.log(err));
+  }, []);
+  console.log(obj)
+
   return (
     <div className={props.className}>
-      <Photo className="img-centered" src={testURL} alt="Test url"/>
+      <Photo className="img-centered" src={obj} alt={obj}/>
     </div>
   );
 }
