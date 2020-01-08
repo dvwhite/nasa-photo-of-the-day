63f285d32eac6f806b6de29c5ee6e44fe7814b6d
David White <dwhite0627@gmail.com>
2020-01-07 19:54:59 -0500

    Image component renders the main image from the source data .url property

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -6,20 +6,22 @@ const apiKey = 'DEMO_KEY';
 const apiURL = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`;
 
 const Photos = (props) => {
-  const [obj, setObj] = useState();
+  const [photoData, setPhotoData] = useState([]);
   useEffect(() => {
     axios
       .get(apiURL)
       .then(res => {
-        setObj(res)
+        setPhotoData(res.data)
       })
       .catch(err => console.log(err));
   }, []);
-  console.log(obj)
+  console.log("PhotoData:", photoData)
+  const obj = photoData;
+  console.log("obj:", obj)
 
   return (
     <div className={props.className}>
-      <Photo className="img-centered" src={obj} alt={obj}/>
+      <Photo className="img-centered" src={photoData.url} alt={photoData.explanation}/>
     </div>
   );
 }
