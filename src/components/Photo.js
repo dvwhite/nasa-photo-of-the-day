1715c8dc4b12f41e75290053222c3b862a0102b2
David White <dwhite0627@gmail.com>
2020-01-07 21:25:32 -0500

    Add and format card content using the API data

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,10 +1,20 @@
 import React from "react";
 
-const Photo = (props) => {
+const Photo = ({className, data}) => {
+  // Date formatting
+  var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+  const formattedDate = new Date(data.date).toLocaleDateString("en-US", options);
+
   return (
-    <a href={props.src}>
-      <img className={props.className} src={props.src} alt={props.alt} />
-    </a>
+    <div className="card">
+      <h3>{formattedDate}</h3>
+      <a href={data.url}>
+        <img className={className} src={data.url} alt={data.title} />
+      </a>
+      <h2>{data.title}</h2>
+      <p class="copyright">Copyright: {data.copyright}</p>
+      <p><span class="bold">Explanation: </span>{data.explanation}</p>
+    </div>
   );
 }
 
