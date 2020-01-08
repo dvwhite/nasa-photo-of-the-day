0c2260a1b8f0d332de2017385c7c56b3f5254b40
David White <dwhite0627@gmail.com>
2020-01-07 19:54:24 -0500

    Make the main image a link to the image

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -2,7 +2,9 @@ import React from "react";
 
 const Photo = (props) => {
   return (
-    <img className={props.className} src={props.src} alt={props.alt} />
+    <a href={props.src}>
+      <img className={props.className} src={props.src} alt={props.alt} />
+    </a>
   );
 }
 
